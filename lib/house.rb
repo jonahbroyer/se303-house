4d0dd164400cbb97856c01e1ec581fa82c2db9c2
Jonah Broyer <jkbsk8er@yahoo.com>
2022-04-06 12:39:01 -0700

    Passed test_line_2 successfully.

--- lib/house.rb
+++ lib/house.rb
@@ -4,6 +4,8 @@ class House
     case number
     when 1
       "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
     end
   end
 
