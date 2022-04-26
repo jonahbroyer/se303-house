6e0849c41d7f25135046c29a5d66285461701030
Jonah Broyer <jkbsk8er@yahoo.com>
2022-04-26 15:44:21 -0700

    Removed case 5 duplication

--- lib/house.rb
+++ lib/house.rb
@@ -10,8 +10,6 @@ class House
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
       "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 5
-      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
       "This is the #{phrase(number)} cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
