fca5906d7a7f78f9117a9c66d1347f6c4ce436fc
Jonah Broyer <jkbsk8er@yahoo.com>
2022-04-26 15:07:26 -0700

    Matched else branch with case 11

--- lib/house.rb
+++ lib/house.rb
@@ -25,13 +25,13 @@ class House
     when 11
       "This is the #{phrase(number)} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is the #{phrase(number)} #{phrase(number - 1)} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is the #{phrase(number)} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 
   def phrase(number)
     if number == 12
-      "horse and the hound and the horn that belonged to the"
+      "horse and the hound and the horn that belonged to the #{phrase(number - 1)}"
     elsif number == 11
       "farmer sowing his corn that kept"
     else
