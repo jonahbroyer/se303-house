10fc3833f5d7b601495c6dc8fcb13060f61b5635
Jonah Broyer <jkbsk8er@yahoo.com>
2022-04-26 15:47:31 -0700

    Removed case 3

--- lib/house.rb
+++ lib/house.rb
@@ -6,8 +6,6 @@ class House
       "This is the house that Jack built.\n"
     when 2
       "This is the malt that lay in the house that Jack built.\n"
-    when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     else
       "This is the #{phrase(number)} malt that lay in the house that Jack built.\n"
     end
