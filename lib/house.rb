99d36956faea94cf7c722714baa93264e1ad8c26
Jonah Broyer <jkbsk8er@yahoo.com>
2022-04-26 15:24:16 -0700

    Removed duplication in else block

--- lib/house.rb
+++ lib/house.rb
@@ -21,7 +21,7 @@ class House
     when 9
       "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is the #{phrase(number)} priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is the #{phrase(number)} man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 
