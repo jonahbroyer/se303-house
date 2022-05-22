0836bb7f68c2bb07de8fa1fb7d58e73795c5e141
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-22 13:48:06 -0700

    Removed phrase function entirely as it was duplication

--- lib/house.rb
+++ lib/house.rb
@@ -19,34 +19,6 @@ class House
     "This is the #{@@clause[number - 1]}house that Jack built.\n"
   end
 
-  def phrase(number)
-    if number < @@clause.length
-      return @@clause[number - 1]
-    elsif number == 12
-      "horse and the hound and the horn that belonged to the #{phrase(number - 1)}"
-    elsif number == 11
-      "farmer sowing his corn that kept the #{phrase(number - 1)}"
-    elsif number == 10
-      "rooster that crowed in the morn that woke the #{phrase(number - 1)}"
-    elsif number == 9
-      "priest all shaven and shorn that married the #{phrase(number - 1)}"
-    elsif number == 8
-      "man all tattered and torn that kissed the #{phrase(number - 1)}"
-    elsif number == 7
-      "maiden all forlorn that milked the #{phrase(number - 1)}"
-    elsif number == 6
-      "cow with the crumpled horn that tossed the #{phrase(number - 1)}"
-    elsif number == 5
-      "dog that worried the #{phrase(number - 1)}"
-    elsif number == 4
-      "cat that killed the #{phrase(number - 1)}"
-    elsif number == 3
-      "rat that ate the #{phrase(number - 1)}"
-    elsif number == 2
-      "malt that lay in the #{phrase(number - 1)}"
-    end
-  end
-
   def recite
     (1..12).map { |number| line(number) }.join("\n")
   end
