5d149f5042fab0f6ed4456aa5ec374a9669e80da
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-22 13:37:07 -0700

    Added line 8 to clause

--- lib/house.rb
+++ lib/house.rb
@@ -8,6 +8,7 @@ class House
     "dog that worried the cat that killed the rat that ate the malt that lay in the ",
     "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
     "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+    "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
   ]
 
   def line(number)
