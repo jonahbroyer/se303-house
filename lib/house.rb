8b95a44a739a53f2ceea4fc156c97e83ca9293de
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-22 13:35:55 -0700

    Added line 6 to clause

--- lib/house.rb
+++ lib/house.rb
@@ -6,6 +6,7 @@ class House
     "rat that ate the malt that lay in the ",
     "cat that killed the rat that ate the malt that lay in the ",
     "dog that worried the cat that killed the rat that ate the malt that lay in the ",
+    "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
   ]
 
   def line(number)
