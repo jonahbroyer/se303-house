f8d03107fbc9460b8b47edbc72434d68131bf76b
Jonah Broyer <jkbsk8er@yahoo.com>
2022-06-02 13:16:39 -0700

    Changed clause to be an instance variable.

--- lib/house.rb
+++ lib/house.rb
@@ -1,23 +1,25 @@
 class House
 
-  @@clause = [
-    "This clause is unknown",
-    "",
-    "malt that lay in the ",
-    "rat that ate the malt that lay in the ",
-    "cat that killed the rat that ate the malt that lay in the ",
-    "dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+  def initialize()
+    @clause = [
+      "This clause is unknown",
+      "",
+      "malt that lay in the ",
+      "rat that ate the malt that lay in the ",
+      "cat that killed the rat that ate the malt that lay in the ",
+      "dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+      "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
   ]
+end
 
   def line(number)
-    "This is the #{@@clause[number]}house that Jack built.\n"
+    "This is the #{@clause[number]}house that Jack built.\n"
   end
 
   def recite
@@ -28,7 +30,7 @@ end
 class Pirate < House
 
   def line(number)
-    "Thar be the #{@@clause[number]}house that Jack built.\n"
+    "Thar be the #{@clause[number]}house that Jack built.\n"
   end
 
 end
