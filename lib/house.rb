fbed1d0220d8d982d3d978dace2f4b06de9c7861
Jonah Broyer <jkbsk8er@yahoo.com>
2022-06-02 13:23:45 -0700

    Adjust clause to be built by phrases using a new method.

--- lib/house.rb
+++ lib/house.rb
@@ -5,21 +5,25 @@ class House
       "This clause is unknown",
       "",
       "malt that lay in the ",
-      "rat that ate the malt that lay in the ",
-      "cat that killed the rat that ate the malt that lay in the ",
-      "dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-      "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-  ]
+      "rat that ate the ",
+      "cat that killed the ",
+      "dog that worried the ",
+      "cow with the crumpled horn that tossed the ",
+      "maiden all forlorn that milked the ",
+      "man all tattered and torn that kissed the ",
+      "priest all shaven and shorn that married the ",
+      "rooster that crowed in the morn that woke the ",
+      "farmer sowing his corn that kept the ",
+      "horse and the hound and the horn that belonged to the ",
+    ]
 end
 
   def line(number)
-    "This is the #{@clause[number]}house that Jack built.\n"
+    "This is the #{build_line(number)}house that Jack built.\n"
+  end
+
+  def build_line(number)
+    number.downto(1).collect { |i| @clause[i] }.join("")
   end
 
   def recite
@@ -30,7 +34,7 @@ end
 class Pirate < House
 
   def line(number)
-    "Thar be the #{@clause[number]}house that Jack built.\n"
+    "Thar be the #{build_line(number)}house that Jack built.\n"
   end
 
 end
