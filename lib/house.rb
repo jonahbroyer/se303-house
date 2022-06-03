5f75a6834715d3006da2d6f02ecc7495fc449f27
Jonah Broyer <jkbsk8er@yahoo.com>
2022-06-03 15:36:45 -0700

    Reordered clause

--- lib/house.rb
+++ lib/house.rb
@@ -9,24 +9,23 @@ class House
 
   def clause
     [
-      "This clause is unknown",
-      "",
-      "malt that lay in the ",
-      "rat that ate the ",
-      "cat that killed the ",
-      "dog that worried the ",
-      "cow with the crumpled horn that tossed the ",
-      "maiden all forlorn that milked the ",
-      "man all tattered and torn that kissed the ",
-      "priest all shaven and shorn that married the ",
-      "rooster that crowed in the morn that woke the ",
-      "farmer sowing his corn that kept the ",
-      "horse and the hound and the horn that belonged to the ",
+      'the horse and the hound and the horn that belonged to',
+      'the farmer sowing his corn that kept',
+      'the rooster that crowed in the morn that woke',
+      'the priest all shaven and shorn that married',
+      'the man all tattered and torn that kissed',
+      'the maiden all forlorn that milked',
+      'the cow with the crumpled horn that tossed',
+      'the dog that worried',
+      'the cat that killed',
+      'the rat that ate',
+      'the malt that lay in',
+      'the house that Jack built',
     ]
   end
 
   def line(number)
-    "#{starting_phrase} the #{build_line(number)}house that Jack built.\n"
+    "#{starting_phrase} #{clause.last(number).join(' ')}.\n"
   end
 
   def build_line(number)
